e4060493c14bc336f9da01a7c3165671ad2aff8b
lnxpsyhi <rommelmatic21@gmail.com>
2024-07-29 22:00:17 +0800

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+
+#[no_mangle]
+fn android_main(app: slint::android::AndroidApp) {
+    slint::android::init(app).unwrap();
+
+    // ... rest of your code ...
+    slint::slint!{
+        export component MainWindow inherits Window {
+            Text { text: "Hello World"; }
+        }
+    }
+    MainWindow::new().unwrap().run().unwrap();
+}
